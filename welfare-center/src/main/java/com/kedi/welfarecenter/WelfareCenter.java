8c9a74a9c54b6cac301c8848f4ab8265c158ab79
dengwei <1194823460@qq.com>
2019-07-22 11:25:33 +0800

    ss

--- welfare-center/src/main/java/com/kedi/welfarecenter/WelfareCenter.java
+++ welfare-center/src/main/java/com/kedi/welfarecenter/WelfareCenter.java
@@ -6,7 +6,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-
+/**
+ * 
+* @Description: 
+* @author: dw 
+* @date: 2019 2019年7月22日 上午9:58:04
+ */
 
 @EnableFeignClients(basePackages = "com.kedi")
 @ComponentScan("com.kedi")
